6b846d273cc8eae474551c33c4f3b40ecf05fb3f
eoryazantseva <eoryazantseva@gmail.com>
2023-08-08 12:10:12 +1000

    created a single-project view and registered URL for this endpoint

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -15,3 +15,14 @@ class ProjectList(APIView):
         if serializer.is_valid():
             serializer.save() # ...tell the serializer to save a new record to the DB,...
             return Response(serializer.data) # ...respond with the JSON detailing what was saved
+        
+
+class ProjectDetail(APIView):
+
+    def get_object(self, pk): # when this view gets called, the front end will be asking for info about a specific project. This method says when the view goes looking for a project, it should use the promary key of that project. 
+        return Project.objects.get(pk=pk) # The view should grab the project with the pk from the db.
+    
+    def get(self, request, pk): # also tells our our view how to handle GET requsests. This method says that the way to do this is to:
+        project = self.get_object(pk) #  get the data from the db
+        serializer = ProjectSerializer(project) # serialize the data to JSON
+        return Response(serializer.data) # return it as a response
