dfe6fc46981adc43932c11123471e934442e24f2
eoryazantseva <eoryazantseva@gmail.com>
2023-08-08 12:26:20 +1000

    added status codes to project list and project details views

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,6 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project
 from .serializers import ProjectSerializer
+from django.http import Http404
+from rest_framework import status
+
 
 class ProjectList(APIView):
 
@@ -14,14 +17,25 @@ class ProjectList(APIView):
         serializer = ProjectSerializer(data=request.data) # ... it should load the data into serializer,...
         if serializer.is_valid():
             serializer.save() # ...tell the serializer to save a new record to the DB,...
-            return Response(serializer.data) # ...respond with the JSON detailing what was saved
-        
+            return Response(  # ...respond with the JSON detailing what was saved and a status code 201 (successfully created)
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response( #if our serialized data is not valid, we are returning a 400 code (bad request)
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
 
 class ProjectDetail(APIView):
 
-    def get_object(self, pk): # when this view gets called, the front end will be asking for info about a specific project. This method says when the view goes looking for a project, it should use the promary key of that project. 
-        return Project.objects.get(pk=pk) # The view should grab the project with the pk from the db.
-    
+    def get_object(self, pk): # when this view gets called, the front end will be asking for info about a specific project. This method says when the view goes looking for a project, it should use the promary key of that project. The view should grab the project with the pk from the db.
+        try: # added try/except block to our get_object method. It tells Python to attempt to retrieve the record from the db. Bit, if it encounters a problem in the process, it raises an HTTP404 error (404 code to the front end)
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            raise Http404
+        
+
     def get(self, request, pk): # also tells our our view how to handle GET requsests. This method says that the way to do this is to:
         project = self.get_object(pk) #  get the data from the db
         serializer = ProjectSerializer(project) # serialize the data to JSON
