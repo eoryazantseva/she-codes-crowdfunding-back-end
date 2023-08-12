8cb9904d666dd22bd8ab74f7eebc1b866ac52b6d
eoryazantseva <eoryazantseva@gmail.com>
2023-08-12 12:00:17 +1000

    deployment settings

--- crowdfunding/run.sh
+++ crowdfunding/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python manage.py migrate
 python manage.py createsuperuser --no-input
-gunicorn --bind :8000 --workers 1 crowdfunding.wsgifly volumes create -a sc-wishfrog -r syd --size 1 dbvol
+gunicorn --bind :8000 --workers 1 crowdfunding.wsgifly
