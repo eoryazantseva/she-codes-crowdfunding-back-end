21bced06244c4f138377ae7c0a423bfc03b35f1e
eoryazantseva <eoryazantseva@gmail.com>
2023-08-12 10:30:43 +1000

    deployment settings

--- crowdfunding/run.sh
+++ crowdfunding/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python manage.py migrate
 python manage.py createsuperuser --no-input
-gunicorn --bind :8000 --workers 1 crowdfunding.wsgifly volumes create -a <app-name> -r syd --size 1 dbvol
+gunicorn --bind :8000 --workers 1 crowdfunding.wsgifly volumes create -a fly-builder-bitter-hill-7413 -r syd --size 1 dbvol
