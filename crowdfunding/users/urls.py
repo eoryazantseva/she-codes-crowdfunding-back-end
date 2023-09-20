b2313db3c9d70fb93be5ae796c6a02b1be417575
eoryazantseva <eoryazantseva@gmail.com>
2023-09-20 16:04:47 +1000

    made changes to CustomUserByUsername view url

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('users/', views.CustomUserList.as_view()),
     path('users/<int:pk>/', views.CustomUserDetail.as_view()),
-    path('users/username/<str:username>/', views.CustomUserByUsername.as_view()),
+    path('users/<str:username>/', views.CustomUserByUsername.as_view())
 ]
